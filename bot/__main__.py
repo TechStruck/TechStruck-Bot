776191ce213b7823d38dc4c94a5d7e2253fe8e24
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2021-01-02 20:19:17 +0530

    Several improvements Added extension loading Added jsk Fixed roles.yaml

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,10 @@
+import os
 from discord.ext import commands
-from bot import TechStruckBot
+from .bot import TechStruckBot
+
+os.environ.setdefault('JISHAKU_HIDE', '1')
+os.environ.setdefault('JISHAKU_RETAIN', '1')
+os.environ.setdefault('JISHAKU_NO_UNDERSCORE', '1')
 
 if __name__ == '__main__':
     import config
--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,23 @@
+import traceback
+from typing import List
 from discord.ext import commands
 
 
 class TechStruckBot(commands.Bot):
-    def __init__(self):
+    def __init__(self, load_extensions=True, loadjsk=True):
         super().__init__(command_prefix='.')
+
+        if load_extensions:
+            self.load_extensions(())
+        if loadjsk:
+            self.load_extension('jishaku')
+
+    def load_extensions(self, extentions: List[str]):
+        for ext in extentions:
+            try:
+                self.load_extension(ext)
+            except Exception as e:
+                traceback.print_exception(type(e), e, e.__traceback__)
 
     async def on_ready(self):
         print("Ready!")
