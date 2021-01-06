b54aa2f5f87f3f9f066f29a2ad30c29e8f5b76aa
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2021-01-06 14:05:15 +0530

    A bit of improvision

--- api/meme.py
+++ api/meme.py
@@ -1,9 +1,14 @@
-from http.server import BaseHTTPRequestHandler
-from praw import Reddit
-from utils.webhook import make_webhook
-from discord import Embed, Color
-from config import config
+import json
 import random
+import traceback
+from http.server import BaseHTTPRequestHandler
+from typing import List
+
+from discord import Color, Embed, Webhook
+from praw import Reddit
+
+from config import config
+from utils.webhook import make_webhook
 
 reddit = Reddit(
     client_id=config['REDDIT_ID'],
@@ -18,25 +23,41 @@ reddit.read_only = True
 
 webhook = make_webhook(config['MEME_CHANNEL_WEBHOOK_URL'])
 
+ALLOWED_FORMATS = ('.jpg', '.gif', '.png', '.jpeg')
+SUBREDDITS = ('memes', 'meme', 'dankmeme', 'me_irl',
+              'dankmemes', 'blursedimages', 'showerthoughts')
+
+
+def send_memes(webhook: Webhook, subreddits: List[str], quantity: int):
+    sent = 0
+    skipped = 0
+    while sent < quantity:
+        meme_subreddit = reddit.subreddit(random.choice(SUBREDDITS))
+        meme = meme_subreddit.random()
+        if not any((meme.url.endswith(i) for i in ALLOWED_FORMATS)):
+            skipped += 1
+            continue
+        embed = Embed(title=meme.title, color=Color.magenta())
+        embed.set_image(url=meme.url)
+        embed.set_footer(
+            text=f'\U0001f44d {meme.ups} \u2502 \U0001f44e {meme.downs}')
+        webhook.send(embed=embed)
+        sent += 1
+    return {'sent': sent, 'skipped': skipped}
+
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        sent = 0
-        while sent < 5:
-            meme_subreddit = reddit.subreddit(random.choice(
-                ('memes', 'MemeEconomy', 'dankmemes', 'dankmemer')))
-            meme = meme_subreddit.random()
-            if not any((meme.url.endswith(i) for i in ('.jpg', '.gif', '.png', '.jpeg'))):
-                continue
-            embed = Embed(title=meme.title, color=Color.greyple())
-            embed.set_image(url=meme.url)
-            embed.set_footer(
-                text=f'\U0001f44d {meme.ups} \u2502 \U0001f44e {meme.downs}')
-            webhook.send(embed=embed)
-            sent += 1
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
+        try:
+            out = send_memes(webhook, SUBREDDITS, 5)
+        except Exception as e:
+            self.send_response(500)
+            out = {'error': traceback.format_exception(
+                type(e), e, e.__traceback__)}
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
 
-        self.wfile.write("Done".encode())
+        self.wfile.write(json.dumps(out).encode())
