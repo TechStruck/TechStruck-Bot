5a05678fdd756eb9967e323180c129217145d4a8
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2021-02-01 21:02:15 +0530

    Fix datetime.datetime.fromisoformat not being available for python <3.7

--- api/main.py
+++ api/main.py
@@ -1,4 +1,10 @@
+import sys
+
 from fastapi import Depends, FastAPI, Header, HTTPException, status
+
+if sys.version_info[1] < 7:
+    from backports.datetime_fromisoformat import MonkeyPatch
+    MonkeyPatch.patch_fromisoformat()
 
 from .dependencies import db_pool
 from .routers import oauth, webhooks
