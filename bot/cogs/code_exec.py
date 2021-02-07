e388781486afc9653301cf3eb0f6bb3bd32a21fd
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2021-02-07 08:18:32 +0530

    Improvement: CodeExec cog - output is shortened to <15 lines <500 chars

--- bot/cogs/code_exec.py
+++ bot/cogs/code_exec.py
@@ -73,7 +73,10 @@ class CodeExec(commands.Cog):
 #            return await ctx.reply("Your output was too long, so here's the pastebin link " + url)
         embed = Embed(
             title=f"Ran your {result['language']} code", color=Color.green())
-        embed.add_field(name="Output", value=(output[:500] + (len(output)>500) * "\n\n<output shortened>") or "<no output>")
+        output = output[:500] + (len(output)>500) * "\n\n**Output shortened**"
+        lines = output.splitlines()
+        output = "\n".join(lines[:15]) + (len(output)>15) * "\n\n**Output shortened**"
+        embed.add_field(name="Output", value=output or "**<No output>**")
 
         await ctx.reply(embed=embed)
 
