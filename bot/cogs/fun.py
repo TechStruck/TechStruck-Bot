144a147ce3852a267d53ba1e2a7b2b43067ff40c
FalseDev <falsedev@zeroCode.local>
2021-02-20 21:31:17 +0530

    Added beers command

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -38,6 +38,31 @@ class Fun(commands.Cog):
             beer_offer = beer_offer + f"\n\nReason: {reason}" if reason else beer_offer
             await msg.edit(content=beer_offer)
 
+    @commands.command()
+    async def beers(self, ctx:commands.Context, *members: Member):
+        if not members:
+            return await ctx.send("You can't have beer with no other person!")
+        for member in members:
+            if member.bot:
+                return await ctx.send("Beer with bots isn't exactly a thing...")
+
+        message = ", ".join(m.display_name for m in members) + "\nYou have been invited for beer \U0001f37b by " + ctx.author.display_name
+
+        msg = await ctx.send(message)
+        await msg.add_reaction("\U0001f37b")
+
+        def check(r, m):
+            return m in members and r.message == msg and str(r.emoji) == "\U0001f37b"
+
+        while True:
+            try:
+                r,_ = await self.bot.wait_for("reaction_add", check=check, timeout=60)
+            except asyncio.TimeoutError:
+                return await msg.edit(content="Ouch, looks like not everyone wants beer now...")
+            else:
+                if set(m.id for m in await r.message.reactions[0].users().flatten()).issuperset(m.id for m in members):
+                    return await msg.edit(content=(", ".join(m.display_name for m in members) + ", " + ctx.author.display_name + " enjoy a lovely beer together \U0001f37b"))
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Fun(bot))
