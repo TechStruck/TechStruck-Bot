92a9432edfddc24175b228c84d4ac33d28a7856e
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-02-27 17:28:33 +0530

    Added token caching for Github cog

--- bot/cogs/github.py
+++ bot/cogs/github.py
@@ -5,6 +5,7 @@ from urllib.parse import urlencode
 from discord import Color, Embed, Member
 from discord.ext import commands
 from jose import jwt
+from cachetools import TTLCache
 
 from config.common import config
 from config.oauth import github_oauth_config
@@ -20,29 +21,44 @@ class Github(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.files_regex = re.compile(r"\s{0,}```\w{0,}\s{0,}")
+        self.token_cache = TTLCache(maxsize=1000, ttl=600)
 
     @property
     def session(self):
         return self.bot.http._HTTPClient__session
 
     async def cog_check(self, ctx: commands.Context):
-        user = await UserModel.get_or_none(id=ctx.author.id)
-        if ctx.command != self.link_github and (
-                user is None or user.github_oauth_token is None):
-            raise GithubNotLinkedError()
-        ctx.user_obj = user
+        token = self.token_cache.get(ctx.author.id)
+        if not token:
+            user = await UserModel.get_or_none(id=ctx.author.id)
+            if ctx.command != self.link_github and (
+                user is None or user.github_oauth_token is None
+            ):
+                raise GithubNotLinkedError()
+            token = user.github_oauth_token
+            self.token_cache[ctx.author.id] = token
+        ctx.gh_token = token
         return True
 
     @commands.command(name="linkgithub", aliases=["lngithub"])
     async def link_github(self, ctx: commands.Context):
         expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=120)
-        url = "https://github.com/login/oauth/authorize?" + urlencode({
-            'client_id': github_oauth_config.client_id,
-            'scope': "gist",
-            'redirect_uri': "https://tech-struck.vercel.app/oauth/github",
-            'state': jwt.encode({'id': ctx.author.id, 'expiry': str(expiry)}, config.secret),
-        })
-        await ctx.author.send(embed=Embed(title="Connect Github", description=f"Click [this]({url}) to link your github account. This link invalidates in 2 minutes"))
+        url = "https://github.com/login/oauth/authorize?" + urlencode(
+            {
+                "client_id": github_oauth_config.client_id,
+                "scope": "gist",
+                "redirect_uri": "https://tech-struck.vercel.app/oauth/github",
+                "state": jwt.encode(
+                    {"id": ctx.author.id, "expiry": str(expiry)}, config.secret
+                ),
+            }
+        )
+        await ctx.author.send(
+            embed=Embed(
+                title="Connect Github",
+                description=f"Click [this]({url}) to link your github account. This link invalidates in 2 minutes",
+            )
+        )
 
     @commands.command(name="creategist", aliases=["crgist"])
     async def create_gist(self, ctx: commands.Context, *, inp):
@@ -62,10 +78,12 @@ class Github(commands.Cog):
         """
         files_and_names = self.files_regex.split(inp)[:-1]
         # Dict comprehension to create the files 'object'
-        files = {name: {"content": content + "\n"} for name,
-                 content in zip(files_and_names[0::2], files_and_names[1::2])}
+        files = {
+            name: {"content": content + "\n"}
+            for name, content in zip(files_and_names[0::2], files_and_names[1::2])
+        }
 
-        req = await self.session.post("https://api.github.com/gists", headers={"Authorization": f"Bearer {ctx.user_obj.github_oauth_token}"}, json={"files": files})
+        req = await self.github_request(ctx, "POST", "/gists", json={"files": files})
 
         res = await req.json()
         # TODO: Make this more verbose to the user and log errors
@@ -75,24 +93,58 @@ class Github(commands.Cog):
     async def github_search(self, ctx: commands.Context, *, term: str):
         # TODO: Docs
 
-        req = await self.session.get("https://api.github.com/search/repositories", headers={"Authorization": f"Bearer {ctx.user_obj.github_oauth_token}"}, params=dict(q=term, per_page=5))
+        req = await self.github_request(
+            ctx, "GET", "/search/repositories", dict(q=term, per_page=5)
+        )
 
         data = await req.json()
-        if not data['items']:
-            return await ctx.send(embed=Embed(title=f"Searched for {term}", color=Color.red(), description="No results found"))
+        if not data["items"]:
+            return await ctx.send(
+                embed=Embed(
+                    title=f"Searched for {term}",
+                    color=Color.red(),
+                    description="No results found",
+                )
+            )
 
-        em = Embed(title=f"Searched for {term}", color=Color.green(), description="\n\n".join([
-            "[{0[owner][login]}/{0[name]}]({0[html_url]})\n{0[stargazers_count]:,} :star:\u2800{0[forks_count]} \u2387\u2800\n{1}".format(result, self.repo_desc_format(result))
-            for result in data['items']
-        ]))
+        em = Embed(
+            title=f"Searched for {term}",
+            color=Color.green(),
+            description="\n\n".join(
+                [
+                    "[{0[owner][login]}/{0[name]}]({0[html_url]})\n{0[stargazers_count]:,} :star:\u2800{0[forks_count]} \u2387\u2800\n{1}".format(
+                        result, self.repo_desc_format(result)
+                    )
+                    for result in data["items"]
+                ]
+            ),
+        )
 
         await ctx.send(embed=em)
 
     @staticmethod
     def repo_desc_format(result):
-        description = result['description']
-        if not description: return ''
-        return description if len(description)<100 else (description[:100] + '...')
+        description = result["description"]
+        if not description:
+            return ""
+        return description if len(description) < 100 else (description[:100] + "...")
+
+    def github_request(
+        self,
+        ctx: commands.Context,
+        req_type: str,
+        endpoint: str,
+        params: dict = None,
+        json: dict = None,
+    ):
+        return self.session.request(
+            req_type,
+            f"https://api.github.com{endpoint}",
+            params=params,
+            json=json,
+            headers={"Authorization": f"Bearer {ctx.gh_token}"},
+        )
+
 
 def setup(bot: commands.Bot):
     bot.add_cog(Github(bot))
