5ab1625c8b33a9354f2f64fc7b42548b4cdd3160
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-06-20 13:13:37 +0530

    Added embed to source command
    embeds are fun aren't they?!

--- bot/cogs/utils.py
+++ bot/cogs/utils.py
@@ -132,7 +132,10 @@ class Utils(commands.Cog):
         else:
             cmd = self.bot.get_command(command)
             if cmd is None:
-                return await ctx.send("No such command found.")
+                return await ctx.send(embed=Embed(
+                            description="No such command found.",
+                            color=0x8ADCED
+                        )
             src = cmd.callback.__code__
             module = cmd.callback.__module__
             filename = src.co_filename
@@ -146,7 +149,10 @@ class Utils(commands.Cog):
         )
 
         url = f"https://github.com/TechStruck/TechStruck-Bot/blob/main/{location}#L{firstline}-L{firstline+lines-1}"
-        await ctx.send(url)
+        await ctx.send(embed=Embed(
+            description=f"Source of {command} can be found [here]({url}).",
+            color=0x8ADCED
+        )
 
 
 def setup(bot: TechStruckBot):
